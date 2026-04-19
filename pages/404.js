efb43bc38675328b8db4bad02c40876f23fcc25f
Shahid Mukhtar <shahid111mukhtar1436@gmail.com>
2026-04-19 14:18:28 +0500

    Nuke pages directory entirely to strictly enforce App Router

--- pages/404.js
+++
@@ -1,8 +0,0 @@
-// pages/404.js
-export default function Custom404() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
-}
