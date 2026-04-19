5f9359c900dd52739f9ac3c4552c3a7d24ff7aa1
Shahid Mukhtar <shahid111mukhtar1436@gmail.com>
2026-04-19 14:19:04 +0500

    Restore aggressive pages deletion in prebuild to fix Hostinger fallback errors

--- clean.js
+++ clean.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const dirsToClean = ['.next'];
+const dirsToClean = ['.next', 'pages'];
 
 dirsToClean.forEach((dir) => {
   const dirPath = path.resolve(process.cwd(), dir);
