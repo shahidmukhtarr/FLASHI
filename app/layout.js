7ee7f2997c846cdad5bcb303ea0c528024adf5ac
Shahid Mukhtar <140239592+shahidmukhtarr@users.noreply.github.com>
2026-04-23 18:28:33 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -42,6 +42,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="-GfeIA9cVZ6cTozbh7fRZd3BDr4LvSkRnRV3_4rdjtw" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6296239062398160" crossOrigin="anonymous"></script>
